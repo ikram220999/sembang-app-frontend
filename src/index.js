4a1d1dfdf1cf13f4d131146a8d49028b5279961b
LAPTOP-K0PEIKNF\ikram <ikramhensem25@gmail.com>
2022-05-31 21:51:52 +0800

    second commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <Routes>
-    <Route pathname="/" exact component={<App />} />
-  </Routes>
+    <App />
   // </React.StrictMode>
 );
 
