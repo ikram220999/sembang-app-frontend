62a468eefd8e3fc47b117a70b6fcbd8f48b3976b
LAPTOP-K0PEIKNF\ikram <ikramhensem25@gmail.com>
2022-06-05 02:10:06 +0800

    add room number and alert for new user

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import "./App.css";
 import io from "socket.io-client";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Modal } from "flowbite-react";
 
 /* This example requires Tailwind CSS v2.0+ */
 
 const socket = io("https://sembang-kari-app.herokuapp.com");
+// https://sembang-kari-app.herokuapp.com
+// http://localhost:3001
 
 function App() {
+  const id = socket.id;
+
   const [text, setText] = useState("");
   const [room, setRoom] = useState("");
+  const [temRoom, setTemRoom] = useState("");
   const [data, setData] = useState([]);
   const [action, setAction] = useState(true);
+  const [newUser, setNewUser] = useState("");
+  const [showNewUser, setShowNewUser] = useState(false);
+  const [modalShow, setModalShow] = useState(true);
 
   console.log("user ", socket.id);
-
-  const id = socket.id;
 
   console.log("data", data);
   console.log(text);
   console.log(room);
+  console.log("new user", newUser);
 
   const sendMessage = () => {
     socket.emit("send_message", { id, text, room });
@@ -27,9 +35,22 @@ function App() {
   };
 
   const joinRoom = () => {
+    setTemRoom(room);
     if (room !== "") {
-      socket.emit("join_room", room);
+      socket.emit("join_room", { id, room });
     }
+  };
+
+  const onClose = () => {
+    setModalShow(false);
+  }
+
+  const newUserJoin = () => {
+    socket.on("new_user_join", (user) => {
+      console.log("asdasd", user);
+      setNewUser(user);
+      setShowNewUser(true);
+    });
   };
 
   useEffect(() => {
@@ -43,16 +64,58 @@ function App() {
         },
       ]);
     });
-  }, [action]);
+
+    newUserJoin();
+  }, []);
+
+  const vModal = () => {
+    return (
+      <React.Fragment>
+        <Modal show={modalShow} onClose={onClose}>
+          <Modal.Header>Instructions</Modal.Header>
+          <Modal.Body className="space-y-6">
+          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Firstly dont expect too much.
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              1. Fill in room number then click join. 
+              <br />
+              2. Start chatting with dudes in the same room
+            </p>
+
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              Thankyou
+            </p>
+
+            
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={() => setModalShow(false)}>Okay !</Button>
+          </Modal.Footer>
+        </Modal>
+      </React.Fragment>
+    );
+  };
+
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      // After 3 seconds set the show value to false
+      setShowNewUser(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeId);
+    };
+  }, [newUser]);
 
   return (
     <div className="container rounded-lg lg:w-6/12 md:w-8/12 shadow-md m-auto my-6 sm:10/12">
-      <div className="container shadow-md w-full mb-3 bg-blue-500 px-6 py-4 rounded-t-lg flex flex-row items-center justify-between">
+      <div className="container shadow-md w-full mb-3 bg-blue-500 px-6 py-4 rounded-t-lg flex flex-row flex-wrap items-center justify-between">
         <b className="text-white text-xl">Sembang kari App</b>
-        <div>
+        <div className="flex flex-row">
           <input
-            className="border-2 border-gray rounded-lg text-gray-500 py-1 px-3 outline-blue-400"
-            type="text"
+            className="border-2 border-gray-300 rounded-lg text-gray-500 py-1 px-3 outline-blue-400"
+            type="number"
             placeholder="room no"
             onChange={(e) => {
               setRoom(e.target.value);
@@ -64,6 +127,13 @@ function App() {
           >
             Join
           </button>
+          {temRoom ? (
+            <div className="w-auto py-2 px-4 mx-2 bg-indigo-800 rounded-xl text-white">
+              <b>{temRoom}</b>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
       {/* <div className="container flex flex-row gap-x-1.5 justify-center items-center">
@@ -80,29 +150,41 @@ function App() {
           Room 4
         </div>
       </div> */}
-
-      <div className="container shadow-md h-96 mt-2 py-3 px-3 overflow-y-scroll">
-        {/* {data.map((da) => (
-        <p>{da.message}</p>
-      ))} */}
-        <div className="container mt-2 py-3 ">
-          {data.map((da) =>
-            da.id === socket.id ? (
-              <div className="w-fit py-3 px-4 pr-10 bg-blue-300 max-w-sm shadow-sm rounded-2xl mb-2">
-                <p>{da.message}</p>
-              </div>
-            ) : (
-              <div className="w-fit py-3 px-4 pr-10 bg-blue-100 max-w-sm shadow-sm rounded-2xl mb-2">
-                <p>{da.message}</p>
-              </div>
-            )
-          )}
+      <div className="relative">
+        <div className="container shadow-md h-96 mt-2 py-3 px-3 overflow-y-scroll">
+          {/* {data.map((da) => (
+          <p>{da.message}</p>
+        ))} */}
+          <div className="container mt-2 py-3 ">
+            {data.map((da) =>
+              da.id === socket.id ? (
+                <div className="w-fit py-3 px-4 pr-10 bg-blue-300 max-w-sm shadow-sm rounded-2xl mb-2">
+                  <p>{da.message}</p>
+                </div>
+              ) : (
+                <div className="w-fit py-3 px-4 pr-10 bg-blue-100 max-w-sm shadow-sm rounded-2xl mb-2">
+                  <p>{da.message}</p>
+                </div>
+              )
+            )}
+          </div>
         </div>
+        {showNewUser ? (
+          <div class="absolute bottom-3 inset-x-1/4 p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex">
+            <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+              New
+            </span>
+            <span class="font-semibold mr-2 text-left flex-auto">
+              <i>{newUser.user_id}</i> has joined the room
+            </span>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
-
       <div className="container m-2">
         <input
-          className="border-2 border-gray rounded-lg py-2 px-4 m-3 w-9/12 text-gray-500 outline-blue-400"
+          className="border-2 border-gray-300 rounded-lg py-2 px-4 m-3 w-9/12 text-gray-500 outline-blue-100"
           type="text"
           placeholder="Message ..."
           value={text}
@@ -117,8 +199,8 @@ function App() {
           Send
         </button>
       </div>
+      {vModal()}
     </div>
-    
   );
 }
 
